b5dff5a302cc2c9cca674ea67eaf0d6c2d6f38c3
JeffersonPereiraDev <jeffersonpereira3@hotmail.com>
2022-01-19 10:40:40 -0300

    Create component PlayerVersus

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ import "./src/styles/elements/base.css"
 
 import BoardGame from "./src/objects/BoardGame";
 import ScoreBoard from "./src/objects/ScoreBoard";
-import CardFrontBack from "./src/components/CardFrontBack";
 
 
 const $root = document.querySelector("#root");
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerVersus from "../../components/PlayerVersus";
 import "./style.css";
 
 function ScoreBoard(){
     return /*html*/`
         <header class = "score-board">
             ${PlayerName("Player1")}
+            ${PlayerVersus()}
             ${PlayerName("Player2")}
         </header>
     `;
