629bc6ef66e9f69178689d148462283357653559
JeffersonPereiraDev <jeffersonpereira3@hotmail.com>
2022-01-20 11:00:44 -0300

    Create component PlayerScore

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,4 +1,5 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
 import PlayerVersus from "../../components/PlayerVersus";
 import "./style.css";
 
@@ -6,7 +7,9 @@ function ScoreBoard(){
     return /*html*/`
         <header class = "score-board">
             ${PlayerName("Player1")}
+            ${PlayerScore(1)}
             ${PlayerVersus()}
+            ${PlayerScore(2)}
             ${PlayerName("Player2")}
         </header>
     `;
